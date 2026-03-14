28bdd0f12cd787aea126e9e99144a55aab988776
Andersonrod <anderson.oliveira1780@gmail.com>
2026-03-14 10:50:05 -0300

    creating main loop conecting menu

---
+++ code/Blackground.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Entity import entinity
+
+
+class BLACKGROUND(entinity):
+    def __init__(self):
+        self.move(): ->none = None
---
+++ code/Menu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import tkinter
+
+
+class Menu:
+    def __init__(self , window):
+        self.window = window
+
+    def run(self):
+        pass
+
